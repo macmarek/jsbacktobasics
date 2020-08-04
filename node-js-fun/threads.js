f8862f03379a4bc3df3ac8d7c4cdfb67c29058d0
Marek Czarnecki <marekczarnecki@Mareks-MacBook-Pro.local>
2020-08-04 23:36:38 +0200

    threads increase th pool size

--- node-js-fun/threads.js
+++ node-js-fun/threads.js
@@ -1,6 +1,8 @@
+process.env.UV_THREADPOOL_SIZE = 16;
+
 const crypto = require("crypto");
 
-const jobsCount = 8;
+const jobsCount = 32;
 const start = Date.now();
 for (let i = 0; i < jobsCount; i++) {
   crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
