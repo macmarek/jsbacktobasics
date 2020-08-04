db18bedc09b148d4bf6a21e1ca4d4909aa3b8e0d
Marek Czarnecki <marekczarnecki@Mareks-MacBook-Pro.local>
2020-08-04 23:25:12 +0200

    threads improved sample

--- node-js-fun/threads.js
+++ node-js-fun/threads.js
@@ -1,24 +1,9 @@
 const crypto = require("crypto");
 
+const jobsCount = 8;
 const start = Date.now();
-crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
-  console.log("1:", Date.now() - start);
-});
-
-crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
-  console.log("2:", Date.now() - start);
-});
-
-crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
-  console.log("3:", Date.now() - start);
-});
-
-crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
-  console.log("4:", Date.now() - start);
-});
-
-// 4 threads in thread pool in libuv by default...
-
-crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
-  console.log("5:", Date.now() - start);
-});
+for (let i = 0; i < jobsCount; i++) {
+  crypto.pbkdf2("pass", "salt", 100000, 512, "sha512", () => {
+    console.log(`${i.toString().padStart(2, "0")}:  ${Date.now() - start}`);
+  });
+}
